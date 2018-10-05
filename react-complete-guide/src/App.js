b910641bdff976c4d786526d43007f290b0f7bcc
ThaoNguyen <thao.nguyentt179@gmail.com>
2018-10-05 22:19:58 +0700

    Section 3: Understanding the Base Features & Syntax

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Stephanie", age: 26}
+    ],
+    otherState: "other state value"
+  }
+
+  switchNameHandler = (newName) =>{
+    console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = "Thao Nguyen";
+    this.setState({
+      persons: [
+        {name: newName, age: 28},
+        {name: "Manu", age: 29},
+        {name: "Stephanie", age: 27}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        {name: "Max", age: 28},
+        {name: event.target.value, age: 29},
+        {name: "Stephanie", age: 26}
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroudColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
-        <header className="App-header">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={() => this.switchNameHandler("Thao Nguyen!!")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}>My Hobbies: Racing
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+        />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +75,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
