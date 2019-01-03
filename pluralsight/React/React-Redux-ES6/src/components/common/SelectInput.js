157884fce9802bfd5269730da9f2d7b8f71f7d6b
ThaoNguyen <thao.nguyentt179@gmail.com>
2019-01-03 17:57:27 +0700

    Async Writes in Redux

--- pluralsight/React/React-Redux-ES6/src/components/common/SelectInput.js
+++ pluralsight/React/React-Redux-ES6/src/components/common/SelectInput.js
@@ -3,7 +3,7 @@ import React, {PropTypes} from 'react';
 const SelectInput = ({name, label, onChange, defaultOption, value, error, options}) => {
 
     return (
-        <div className='form-group'>
+        <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <div className="field">
                 <select 
@@ -12,9 +12,7 @@ const SelectInput = ({name, label, onChange, defaultOption, value, error, option
                     onChange={onChange}
                     className="form-control">
                         <option value="">{defaultOption}</option>
-                        {options.map((option) => {
-                            return <option key={option.value} value={option.value}>{option.text}</option>;
-                        })}
+                        {options.map((option) => {return <option key={option.value} value={option.value}>{option.text}</option>;})}
                 </select>
                 {error && <div className="alert alert-danger">{error}</div>}
             </div>
--- pluralsight/React/React-Redux-ES6/src/components/course/ManageCoursePage.js
+++ pluralsight/React/React-Redux-ES6/src/components/course/ManageCoursePage.js
@@ -3,38 +3,63 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import authorReducer from '../../reducers/authorReducer';
 
 class ManageCoursePage extends React.Component{
     constructor(props, context){
         super(props, context);
 
         this.state = {
-            course: Object.assign({}, this.props.course),
+            course: Object.assign({}, props.course),
             errors: {}
         };
-    };
+
+        this.updateCourseState = this.updateCourseState.bind(this);
+        this.saveCourse = this.saveCourse.bind(this);
+    }
+
+    updateCourseState(event){
+        const field = event.target.name;
+        let course = this.state.course;
+        course[field] = event.target.value;
+        return this.setState({course: course});
+    }
+
+    saveCourse(event){
+        event.preventDefault();
+        this.props.actions.saveCourse(this.state.course);
+    }
 
     render(){
         return(
-            <div>
-                <CourseForm 
-                    allAuthors={[]}
+            <CourseForm 
+                    allAuthors={this.props.authors}
+                    onChange={this.updateCourseState}
+                    onSave={this.saveCourse}
                     course={this.state.course}
                     errors={this.state.errors} 
-                />
-            </div> 
+                /> 
         );
     }
 }
 
-ManageCoursePage.PropTypes = {
+ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps){
     let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return{
+            value: author.id,
+            text: author.firstName + ' ' + author.lastName
+        };
+    });
     return {
-        course: course
+        course: course,
+        authors: authorsFormattedForDropdown
     };
 }
 
