3bee1365c3dd91f759955ccd5f114d097d4994f2
ThaoNguyen <thao.nguyentt179@gmail.com>
2019-01-04 12:34:24 +0700

    Async Status and Error Handling

--- pluralsight/React/React-Redux-ES6/src/actions/ajaxStatusActions.js
+++ pluralsight/React/React-Redux-ES6/src/actions/ajaxStatusActions.js
@@ -1,5 +1,10 @@
 import * as types from './actionTypes';
 
 export function beginAjaxCall(){
-    return {type: type.BEGIN_AJAX_CALL};
+    return {type: types.BEGIN_AJAX_CALL};
 }
+
+export function ajaxCallError(){
+    return {type: types.AJAX_CALL_ERROR};
+}
+
